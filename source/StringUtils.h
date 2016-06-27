4fb76ca9a8dc6a71757c572c0398e99cc656729d
Jamie Hoffmann <jamsarhoff@hotmail.com>
2016-06-28 01:42:56 +1000

    oops forgot to add updated project files

---
+++ source/StringUtils.h
@@ -0,0 +1,19 @@
+#ifndef __STRINGUTILSH_GUARD
+#define __STRINGUTILSH_GUARD
+
+#include <string>
+
+namespace JH
+{
+    std::string intToString(long long int i)
+    {
+
+    }
+    long long int stringToInt(std::string s)
+    {
+
+    }
+}
+
+
+#endif // __STRINGUTILSH_GUARD
