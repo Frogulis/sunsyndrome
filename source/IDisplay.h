d87a2b9cacca1642997fa68a12bcee4176cfcadf
Jamie Hoffmann <jamsarhoff@hotmail.com>
2016-07-29 15:45:52 +1000

    Fixed IDisplay.h AGAIN

--- source/IDisplay.h
+++ source/IDisplay.h
@@ -19,11 +19,8 @@ public:
         //
     }
     virtual void draw() =0; //this should take the 3D space object
-<<<<<<< HEAD
     virtual void changeOffset(float x, float y) = 0;
-=======
     virtual void setSpace(Space* space) =0;
->>>>>>> 966dc2addb2e6ae5b7ccfee62eeab0d12748ff73
 };
 
 #endif // __IDISPLAYH_GUARD
